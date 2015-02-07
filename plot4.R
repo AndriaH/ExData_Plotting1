77c3af900761e99a2d6a643f0716b802b803122f
mariahall32@yahoo.com <mariahall32@yahoo.com>
2015-02-07 03:07:10 -0600

    plots for project 1

--- plot4.R
+++ plot4.R
@@ -1,9 +1,15 @@
-###
-# Exploratory Data Analysis: Project 1
-# Requirments: Examine how household energy usage varies
-# over a 2-day period, that is, from the period "
-###
-# Check required R libraries
+##################################################################
+##  Exploratory Data Analysis: Project 1                        ##
+##                                                              ##
+##  Description: Measursements of electric power consumption in ##
+##  1 household with a one-minute sampling rate over a period   ##
+##  of almost 4 years.  Different electrical quantities and     ##
+##  some sub_metering values are available. The dataset has     ##
+##  2075259 rows, 9 columns.                                    ##        
+##  Requirments: Examine how household energy usage varies      ##
+##  over a 2-day period, that is, from the period " 2007-02-01  ## 
+##  and 2007-02-02".                                            ##
+##################################################################
 
 if (!require("data.table")) {
         install.packages("data.table")
@@ -24,7 +30,7 @@ dataSet$Date <- as.Date(dataSet$Date, "%d/%m/%Y")
 # subset the dataSet for data between '2007-02-01' and '2007-02-02'
 dateSet <- subset(dataSet, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-# remove object dataSet from active environment
+# remove the large object dataSet from active environment
 remove(dataSet)
 
 ## converting Date and Time variables into "POSIXct", "POSIXt" classes
@@ -32,8 +38,8 @@ datetime <- paste(dateSet$Date, dateSet$Time)
 dateSet$DateTime <- as.POSIXct(datetime)
 
 
-# set margins for Plot 
-par(mfrow= c(2, 2), mar=c(4,4,2,2), oma=c(2,4,4,2))
+# set margins for Plot 4
+par(mfrow= c(2, 2), mar=c(4,4,2,2))
 
 # Plot 4
 with(dateSet, {
@@ -45,8 +51,8 @@ with(dateSet, {
           ylab="Energy sub metering", xlab="")
      lines(Sub_metering_2 ~ DateTime,col='Red')
      lines(Sub_metering_3 ~ DateTime,col='Blue')
-     legend("topright", col=c("black", "red", "blue"), lwd=2, lty=1, bty="n",
-           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+     legend("topright", cex=0.5, adj = c(0, -.3),legend=c("Sub_metering_1", "Sub_metering_2", 
+        "Sub_metering_3"), col=c("black","red","blue"), lty=1,lwd=2, x.intersp=1, bty="n")
      plot(Global_reactive_power ~ DateTime, type="l",
           ylab="Global_reative_power", xlab="datetime")
 })
@@ -59,4 +65,3 @@ dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
 
 ####################### end of code #######################
-
